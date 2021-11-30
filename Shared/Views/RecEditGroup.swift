cf15b7e2ca0d55648965b801e241c874febc17ec
John <junesunp@andrew.cmu.edu>
2021-11-30 14:40:49 -0500

    Added Group Description and Edit

---
+++ Shared/Views/RecEditGroup.swift
@@ -0,0 +1,37 @@
+
+// RecEditGroup.swift
+// CampusConnect (iOS)
+//
+// Created by Thomas Choi on 11/26/21.
+//
+import SwiftUI
+struct RecEditGroup: View {
+    var group: Group
+    @State var name = ""
+    @State var description = ""
+    @Environment(\.dismiss) var dismiss
+    @ObservedObject var recViewModel = RecruitersViewModel()
+    var body: some View {
+        VStack {
+            Text("Edit  + group.Name").font(.title)
+            TextField("Group Name", text: $name)
+                .padding()
+                .background(Color(.secondarySystemBackground))
+            TextField("Description", text: $description)
+                .padding()
+                .background(Color(.secondarySystemBackground))
+            Button (action: {
+                recViewModel.recEditGroup(curGroup: group, name: name, des: description);
+                dismiss()
+            }, label: {
+                Text("Done Editing")
+                    .foregroundColor(Color.white)
+                    .frame(width: 200, height: 50)
+                    .cornerRadius(8)
+                    .background(Color.blue)
+            })
+        }
+        .padding()
+        //Spacer()
+    }
+}
--- Shared/Views/RecGroupDetail.swift
+++ Shared/Views/RecGroupDetail.swift
@@ -1,59 +1,78 @@
 //
-//  RecGroupDetail.swift
-//  CampusConnect (iOS)
+// RecGroupDetail.swift
+// CampusConnect (iOS)
 //
-//  Created by Thomas Choi on 11/4/21.
+// Created by Thomas Choi on 11/4/21.
 //
-
 import SwiftUI
 import FirebaseFirestore
-
 struct RecGroupDetail: View {
-  @ObservedObject var groupViewModel = GroupsViewModel()
-  var group: Group
-  let width = UIScreen.main.bounds.width * 0.75
-    
-
-  var body: some View {
-      Section(header: Text(group.Name).font(.headline)){
-          if groupViewModel.students.count == 0 {
-              Text("No students in group")
-          }
-          
-          else {
-              VStack{
-                  List{
-                      ForEach(groupViewModel.students) { student in
-                          NavigationLink(destination: RecStudentDetail(student: student, group: group)) {
-                              RecStudentRow(student: student)
-                          }
-                          
-                      }
-                      
-                  }
-              }
-              
-          }
-      }.onAppear(perform: {groupViewModel.fetchStudents(group: group) } )
-       .navigationBarItems(trailing:
-        NavigationLink(destination: Scanner(group: group)) {
-          Image(systemName: "plus")
+    @ObservedObject var groupViewModel = GroupsViewModel()
+    var group: Group
+    let width = UIScreen.main.bounds.width * 0.75
+    @State private var editGroupSheet = false
+    var body: some View {
+        VStack {
+            if groupViewModel.students.count == 0 {
+                Text("No students in group")
+            }
+            else {
+                //Text("Group Information").font(.title3)
+                Section(header: Text("Group Information")){
+                    Spacer().frame(height: 10)
+                    HStack{
+                        Text("No. of Students:").fontWeight(.bold)
+                            .padding(.leading)
+                        Spacer()
+                        Text("\(groupViewModel.students.count)")
+                            .padding(.trailing)
+                    }
+                    Spacer().frame(height: 5)
+                    HStack{
+                        Text("Last Updated:").fontWeight(.bold)
+                            .padding(.leading)
+                        Spacer()
+                        Text(group.Updated, format: .dateTime.month().day().year())
+                            .padding(.trailing)
+                    }
+                }
+                List{
+                    ForEach(groupViewModel.students) { student in
+                        NavigationLink(destination: RecStudentDetail(student: student, group: group)) {
+                            RecStudentRow(student: student)
+                        }
+                    }
+                }
+            }
+        }.navigationBarTitle(group.Name)
+            .onAppear(perform: {groupViewModel.fetchStudents(group: group) } )
+            .navigationBarItems(trailing:
+                                    HStack{
+                NavigationLink(destination: Scanner(group: group)) {
+                    Image(systemName: "plus")
+                };
+                Button("Edit") {
+                    editGroupSheet.toggle()
+                }
+                .sheet(isPresented: $editGroupSheet) {
+                    RecEditGroup(group: group)
+                }
+            }
+            )
+        Spacer()
+        if group.Active{
+            Button(action: { groupViewModel.deactivateGroup(group: group) }){
+                Text("Deactivate Group").foregroundColor(Color(.red))
+                    .frame(width: 600, height: 100)
+                    .background(Color(.clear))
+            }
         }
-       )
-      Spacer()
-      if group.Active{
-          Button(action: { groupViewModel.deactivateGroup(group: group) }){
-              Text("Deactivate Group").foregroundColor(Color(.red))
-                  .frame(width: 600, height: 100)
-                  .background(Color(.clear))
-          }
-      }
-      if !group.Active{
-          Button(action: { groupViewModel.activateGroup(group: group) }){
-              Text("Activate Group").foregroundColor(Color(.green))
-                  .frame(width: 600, height: 100)
-                  .background(Color(.clear))
-          }
-      }
-  }
+        if !group.Active{
+            Button(action: { groupViewModel.activateGroup(group: group) }){
+                Text("Activate Group").foregroundColor(Color(.green))
+                    .frame(width: 600, height: 100)
+                    .background(Color(.clear))
+            }
+        }
+    }
 }
